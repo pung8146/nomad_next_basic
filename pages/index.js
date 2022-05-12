65e6db786650e0f7ac717040cc005f3974b627b1
pung8146 <pung8146@naver.com>
2022-05-12 15:47:47 +0900

    API_KEY 숨기기 / redirects URL변경됨 / rewrites URL이 변경되지않음 추가

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "5c0c3c5ae53bf94d5f6fab131178fde1";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
