a4d1a35ee2088d7e02f425bd0fdd0a203f2ef408
pung8146 <pung8146@naver.com>
2022-05-12 14:38:25 +0900

    nextJs 를 이용한 Movie Copy app 글로벌CSS까지 적용하였음

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+
+export default function About() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>hi About</h1>
+    </div>
+  );
+}
