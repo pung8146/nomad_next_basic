f3677cd1d308569649604bfd71b28fc09006c745
pung8146 <pung8146@naver.com>
2022-05-27 12:49:44 +0900

    server side rendering을 할것인지 => 즉 데이터가 유효할 때 화면이 보여지게할것인지

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading..</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -43,3 +35,17 @@ export default function Home() {
     </div>
   );
 }
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
+
+// server side rendering을 할것인지 => 즉 데이터가 유효할 때 화면이 보여지게할것인지
+// loading화면을 보여주고 그다음 화면을 보여줄것인지
